cd55e0c81520f849eb802cbf6b9246633ebe26d5
surdubob <alex.surdubob@gmail.com>
2019-11-02 19:10:35 +0200

    Added homework for lab 2

---
+++ Homeworks/lab2-rgbpots/rgb_pots.ino
@@ -0,0 +1,35 @@
+const int potR = A0, potG = A1, potB = A2;
+const int ledR = 6, ledG = 5, ledB = 3;
+
+int r = 0, g = 0, b = 0;
+
+void setup() {
+  pinMode(potR, INPUT);
+  pinMode(potG, INPUT);
+  pinMode(potB, INPUT);
+
+  pinMode(ledR, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  pinMode(ledB, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  r = map(analogRead(potR), 0, 1023, 0, 255);
+  g = map(analogRead(potG), 0, 1023, 0, 255);
+  b = map(analogRead(potB), 0, 1023, 0, 255);
+
+  Serial.print("R: ");
+  Serial.print(r);
+  Serial.print("  G: ");
+  Serial.print(g);
+  Serial.print("  B: ");
+  Serial.println(b);
+  
+
+  analogWrite(ledR, r);
+  analogWrite(ledG, g);
+  analogWrite(ledB, b);
+  delay(10);
+}
